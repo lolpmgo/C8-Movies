47e019bffea62403fa9e1534df340f2eaf6a963c
lolpmgo <56176402+lolpmgo@users.noreply.github.com>
2019-11-18 15:12:19 -0800

    1
    1

--- MovieListAL.java
+++ MovieListAL.java
@@ -17,7 +17,8 @@ public class MovieListAL
      */
     public MovieListAL(Movie[] movies)
     {
-
+        for(int i = 0; i < movies.length; i++)
+            pool.add(movies[i]);
     }
 
     public ArrayList<Movie> getPool()
@@ -31,7 +32,11 @@ public class MovieListAL
      */
     public ArrayList<Movie> getByStudio(String studio)
     {
-        return null;
+        ArrayList<Movie> r = new ArrayList<Movie>();
+        for(int i = 0; i < pool.size(); i++)
+            if(pool.get(i).getStudio().equals(studio))
+                r.add(pool.get(i));
+        return r;
     }
     
     
